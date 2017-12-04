d5e8151d0fd86ca1750107f8a9d424f99ade283e
Shu Tamegai <tamegaishu1995@gmial.com>
2017-12-04 14:41:48 +0900

    wiki commit

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -381,7 +381,7 @@
   ;; 2●インストールディレクトリを設定する 初期値は ~/.emacs.d/auto-install/
   (setq auto-install-directory "~/.emacs.d/elisp/")
   ;; EmacsWikiに登録されているelisp の名前を取得する
-  ;;(auto-install-update-emacswiki-package-name t)
+  ;(auto-install-update-emacswiki-package-name t)
   ;; 必要であればプロキシの設定を行う
   ;; (setq url-proxy-services '(("http" . "localhost:8339")))
   ;; 3●install-elisp の関数を利用可能にする
